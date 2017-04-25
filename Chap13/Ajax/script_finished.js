010763d4d2b7e7f21c164cd302671d4921cd12fc
musichopin <merepianist@hotmail.com>
2017-04-25 18:54:06 +0300

    .

--- Chap13/Ajax/script_finished.js
+++ Chap13/Ajax/script_finished.js
@@ -11,19 +11,26 @@ if (window.XMLHttpRequest) {  // does it exist? we're in Firefox, Safari etc.
 }
 
 // 2: create an event handler for our request to call back
+// **we set up listening before we asked the question below with send method, 
+// since its possible that response wud be sent back before we fully set up the listener code
+// (send metodu sonrasında response oluyor diye düşün)**
 myRequest.onreadystatechange = function(){
     console.log("We were called!");
     console.log(myRequest.readyState);
     if (myRequest.readyState === 4) {
         var p = document.createElement("p");
         var t = document.createTextNode(myRequest.responseText);
-        p.appendChild(t);
+        p.appendChild(t); // alt: p.innerHTML = myRequest.responseText
         document.getElementById("mainContent").appendChild(p);
+        // alt:
+        // document.getElementById("mainContent").innerHTML = myRequest.responseText;
     }
 };
 
-// open and send it
+// 3: open the file (normally a php file)
 myRequest.open('GET', 'simple.txt', true);
+
+// 4: send the request
 // any parameters?
 myRequest.send(null);
 
